ed6073624e15ddf7953208d5a64507b884a95ffc
brandon <brandon_charest@yahoo.com>
2016-04-02 19:16:20 -0400

    init commit

---
+++ desktop/src/com/brandoncharest/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.brandoncharest.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.brandoncharest.game.FlappyBirdDemo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlappyBirdDemo(), config);
+	}
+}
---
+++ html/src/com/brandoncharest/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.brandoncharest.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.brandoncharest.game.FlappyBirdDemo;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new FlappyBirdDemo();
+        }
+}
