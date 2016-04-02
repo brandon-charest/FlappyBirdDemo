b5517488129368b41baa3fe1877d3007bbf1c297
brandon <brandon_charest@yahoo.com>
2016-04-02 19:14:05 -0400

    init commit

---
+++ android/src/com/brandoncharest/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.brandoncharest.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.brandoncharest.game.FlappyBirdDemo;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FlappyBirdDemo(), config);
+	}
+}
