30bd4503aa4b7864d5c1e31cc206847def9ba988
brandon <brandon_charest@yahoo.com>
2016-04-02 20:04:19 -0400

    created Game State Manager, States to handle all game states, and Menu screen state

--- desktop/src/com/brandoncharest/game/desktop/DesktopLauncher.java
+++ desktop/src/com/brandoncharest/game/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.brandoncharest.game.FlappyBirdDemo;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width=FlappyBirdDemo.WIDTH;
+		config.height=FlappyBirdDemo.HEIGHT;
+		config.title=FlappyBirdDemo.TITLE;
 		new LwjglApplication(new FlappyBirdDemo(), config);
 	}
 }
